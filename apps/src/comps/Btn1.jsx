047bf66f3d6f69cc42f6df7cfa72832e6df8a134
seo jeong woo <seojeongwoo0902@r2ware.com>
2024-03-05 14:25:26 +0900

    Chatting 화면 디자인 수정 후

---
+++ apps/src/comps/Btn1.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Btn1 = styled.button`
+    box-sizing: border-box;
+    border: 2px solid currentColor;
+    border-radius: 3rem;
+    background-color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    overflow: hidden;
+    padding: 20px;
+    position: relative;
+    text-decoration: none;
+    transition: .2s transform ease-in-out;
+    will-change: transform;
+    z-index: 0;
+    width: ${props => props.width};
+    height: ${props => props.height};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+
+    &::after {
+        background-color: green;
+        border-radius: 3rem;
+        content: '';
+        display: block;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        transform: translate(-100%, 0) rotate(10deg);
+        transform-origin: top left;
+        transition: .2s transform ease-out;
+        will-change: transform;
+        z-index: -1;
+    }
+
+    &:hover::after {
+        transform: translate(0, 0);
+    }
+
+    &:hover {
+        border: 2px solid transparent;
+        color: black;
+        transform: scale(1.05);
+        will-change: transform;
+    }
+`;
--- apps/src/pages/Chatting.jsx
+++ apps/src/pages/Chatting.jsx
@@ -3,12 +3,19 @@ import {useLocation} from "react-router-dom";
 // useLocation이 react-router-dom에서 가져와야 하며,
 // react-router에서 가져오는 것이 아닙니다.
 import useWebSocket from 'react-use-websocket';
-import {Wrapper} from "../comps/comps";
+// import {Wrapper} from "../comps/comps";
 import styled from "styled-components";
-import {Btn1} from "../comps/Btn1";  //웹소켓 라이브러리를 사용 합니다.
+import {Btn1} from "../comps/Btn1";
+import {useIdStore} from "../state/Session";
+// import Smartphone from "./Smartphone";
+
+//웹소켓 라이브러리를 사용 합니다.
 
 
 function Chatting() {
+    const {id} = useIdStore()
+
+
     let {state: {value, _id}} = useLocation();
     const [socketUrl,] = useState(`ws://localhost:8001/wsocket?id=${_id}`);
     const {sendMessage, lastMessage} = useWebSocket(socketUrl);  //웹소캣 라이브러리인 useWebSocket 입니다.
@@ -25,6 +32,7 @@ function Chatting() {
             message: message
         }
         sendMessage(JSON.stringify(msg))
+        setMessage('')
     }
 
     //메시지에 대한 변화에 대해서 정의 합니다.
@@ -53,47 +61,101 @@ function Chatting() {
         }
         sendMessage(JSON.stringify(join))
     }, [value, _id, sendMessage])
+    console.log(messageHistory)
+
+    const AdjustPositionWrapper = styled.div`
+        display: flex;
+        justify-content: ${props => props._id === id ? 'flex-end' : 'flex-start'};
+    `
 
     return (
-        <Wrapper>
-
-
-            <div className='col-md-12 border m-4'>
-                <div>전체 메시지</div>
-                <ul>
-                    {messageHistory.map(
-                        (message, idx) => {
-                            if (message.result) {
-                                let desc = '가 나갔습니다.'
-                                if (message.result === 'someIn') {
-                                    desc = '가 들어왔습니다.'
-                                }
-                                return <div key={idx} className='form-control'>{message._id}{desc}</div>
+        <WholeCase>
+            <Content>
+                {messageHistory.map(
+                    (message, idx) => {
+                        if (message.result) {
+                            let desc = '가 나갔습니다.'
+                            if (message.result === 'someIn') {
+                                desc = '가 들어왔습니다.'
                             }
-                            return <div key={idx} className='form-control'>
-                                <strong>{message._id}</strong> : {message.message}
-                            </div>
+                            return <InNOutCell key={idx}>{message._id}{desc}</InNOutCell>
                         }
-                    )}
-                </ul>
-            </div>
+                        return <AdjustPositionWrapper key={idx} _id={message._id}>
+                            <ChatCell>
+                                <strong>{message._id}</strong> : {message.message}
+
+                            </ChatCell>
+                        </AdjustPositionWrapper>
+                    }
+                )}
+            </Content>
             <ChattingBar>
                 <input type='text' onChange={onMessage} value={message} placeholder='메시지를 입력하세요'
                        className='form-control'/>
                 <Btn1 width='110px' height='40px' onClick={sendMsg}> send</Btn1>
             </ChattingBar>
-
-        </Wrapper>
+        </WholeCase>
     );
 }
+
+
+const WholeCase = styled.div`
+    margin-top: 100px;
+    width: 360px; // 스마트폰 화면의 너비
+    height: 600px; // 스마트폰 화면의 높이
+    background-color: #fff; // 화면 배경색
+    border: 6px solid #000; // 화면 테두리
+    border-radius: 36px; // 테두리 둥글기
+    overflow: hidden; // 테두리 밖으로 넘어가는 내용을 숨김
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Content = styled.div`
+    width: 100%;
+    height: 90%;
+    overflow-y: scroll;
+    padding: 20px;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px
+`;
+
+
+const InNOutCell = styled.div`
+    width: 100%;
+    height: 40px;
+    border: 1px solid black;
+    padding: 3px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+`
+
+const ChatCell = styled.div`
+    width: 70%;
+    height: 40px;
+    border: 1px solid black;
+    padding: 3px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 const ChattingBar = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    width: 300px;
+    width: 100%;
+    height: 10%;
     margin-bottom: 10px;
     gap: 10px;
+
 `
 
 export default Chatting;
